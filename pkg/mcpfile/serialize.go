beb1fb6963459e82834eca27b02c914df0009f5f
Calum Murray <cmurray@redhat.com>
2025-09-15 17:20:04 -0400

    refactor: migrate mcpfile package to new MCP/jsonschema packages and invocation architecture
    Signed-off-by: Calum Murray <cmurray@redhat.com>

--- pkg/mcpfile/serialize.go
+++ pkg/mcpfile/serialize.go
@@ -2,55 +2,20 @@ package mcpfile
 
 import (
 	"encoding/json"
-	"fmt"
 )
 
 func (t *Tool) MarshalJSON() ([]byte, error) {
 	type Doppleganger Tool
 
-	type DummyInvocation struct {
-		Http *HttpInvocation `json:"http,omitempty"`
-		Cli  *CliInvocation  `json:"cli,omitempty"`
-	}
 	tmp := &struct {
-		Invocation DummyInvocation `json:"invocation"`
+		Invocation map[string]json.RawMessage `json:"invocation"`
 		*Doppleganger
 	}{
+		Invocation:   make(map[string]json.RawMessage),
 		Doppleganger: (*Doppleganger)(t),
 	}
 
-	if http, ok := t.Invocation.(*HttpInvocation); ok {
-		tmp.Invocation.Http = http
-	} else if cli, ok := t.Invocation.(*CliInvocation); ok {
-		tmp.Invocation.Cli = cli
-	}
-
-	return json.Marshal(tmp)
-}
-
-func (h *HttpInvocation) MarshalJSON() ([]byte, error) {
-	type Doppleganger HttpInvocation
-
-	tmp := &struct {
-		URL string `json:"url"`
-		*Doppleganger
-	}{
-		Doppleganger: (*Doppleganger)(h),
-	}
-
-	formattedParams := []any{}
-	for _, p := range h.pathParameters {
-		formattedParams = append(formattedParams, fmt.Sprintf("{%s}", p))
-	}
-
-	urlBytes := []byte(h.URL)
-	for i := range len(urlBytes) {
-		if urlBytes[i] == '%' && i+1 < len(urlBytes) {
-			urlBytes[i+1] = 's'
-		}
-	}
-
-	tmp.URL = fmt.Sprintf(string(urlBytes), formattedParams...)
+	tmp.Invocation[t.InvocationType] = t.InvocationData
 
 	return json.Marshal(tmp)
 }
