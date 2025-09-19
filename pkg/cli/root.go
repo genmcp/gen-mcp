53bce0739ad6a7cc944236317bab2377fdb59e00
Calum Murray <cmurray@redhat.com>
2025-09-19 09:49:26 -0400

    feat: add version command to cli
    Signed-off-by: Calum Murray <cmurray@redhat.com>

--- pkg/cli/root.go
+++ pkg/cli/root.go
@@ -7,12 +7,20 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var cliVersion string
+
 var rootCmd = &cobra.Command{
 	Use:   "genmcp",
 	Short: "genmcp manages gen-mcp servers, and their configuration",
 }
 
-func Execute() {
+func Execute(version string) {
+	if version == "" {
+		cliVersion = "development"
+	} else {
+		cliVersion = version
+	}
+
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
