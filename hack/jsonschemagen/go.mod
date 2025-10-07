a35dcd2f97fbd843d5e3d2d1edbf1edf1a83b81d
Ali Ok <aliok@redhat.com>
2025-10-07 12:21:42 +0300

    Generate JSON schema for MCP file

---
+++ hack/jsonschemagen/go.mod
@@ -0,0 +1,21 @@
+module github.com/genmcp/gen-mcp/hack/jsonschemagen
+
+go 1.24.7
+
+require (
+	github.com/genmcp/gen-mcp v0.0.0
+	github.com/invopop/jsonschema v0.13.0
+)
+
+require (
+	github.com/bahlo/generic-list-go v0.2.0 // indirect
+	github.com/buger/jsonparser v1.1.1 // indirect
+	github.com/google/jsonschema-go v0.3.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/wk8/go-ordered-map/v2 v2.1.9-0.20240815153524-6ea36470d1bd // indirect
+	go.yaml.in/yaml/v2 v2.4.2 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	sigs.k8s.io/yaml v1.6.0 // indirect
+)
+
+replace github.com/genmcp/gen-mcp => ../..
---
+++ hack/jsonschemagen/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/invopop/jsonschema"
+
+	"github.com/genmcp/gen-mcp/pkg/mcpfile"
+)
+
+func main() {
+	reflector := new(jsonschema.Reflector)
+	schema := reflector.Reflect(&mcpfile.MCPFile{})
+
+	schemaJSON, err := json.MarshalIndent(schema, "", "  ")
+	if err != nil {
+		log.Fatalf("Failed to marshal schema: %v", err)
+	}
+
+	// write the schema to a file
+	outPath := filepath.Join("..", "..", "mcpfile-schema.json")
+	if err := os.WriteFile(outPath, schemaJSON, 0644); err != nil {
+		log.Fatalf("Failed to write schema to file: %v", err)
+	}
+}
