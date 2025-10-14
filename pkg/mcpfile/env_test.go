751813fb3cb140dd015182dd33145bc5c96f9c14
Calum Murray <cmurray@redhat.com>
2025-10-14 13:03:52 -0400

    feat: add env overrider to mcpfile
    Signed-off-by: Calum Murray <cmurray@redhat.com>

---
+++ pkg/mcpfile/env_test.go
@@ -0,0 +1,102 @@
+package mcpfile
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEnvOverrides(t *testing.T) {
+	tt := map[string]struct {
+		initialRuntime  *ServerRuntime
+		expectedRuntime *ServerRuntime
+		env             map[string]string
+		expectErr       bool
+	}{
+		"no overrides": {
+			initialRuntime: &ServerRuntime{
+				TransportProtocol: "streamablehttp",
+				StreamableHTTPConfig: &StreamableHTTPConfig{
+					Port: 8080,
+				},
+			},
+			expectedRuntime: &ServerRuntime{
+				TransportProtocol: "streamablehttp",
+				StreamableHTTPConfig: &StreamableHTTPConfig{
+					Port: 8080,
+				},
+			},
+		},
+		"override transport protocol": {
+			initialRuntime: &ServerRuntime{
+				TransportProtocol: "streamablehttp",
+				StreamableHTTPConfig: &StreamableHTTPConfig{
+					Port: 8080,
+				},
+			},
+			expectedRuntime: &ServerRuntime{
+				TransportProtocol: "stdio",
+				StreamableHTTPConfig: &StreamableHTTPConfig{
+					Port: 8080,
+				},
+			},
+			env: map[string]string{
+				"GENMCP_TRANSPORTPROTOCOL": "stdio",
+			},
+		},
+		"override nested port": {
+			initialRuntime: &ServerRuntime{
+				TransportProtocol: "streamablehttp",
+				StreamableHTTPConfig: &StreamableHTTPConfig{
+					Port: 8080,
+				},
+			},
+			expectedRuntime: &ServerRuntime{
+				TransportProtocol: "streamablehttp",
+				StreamableHTTPConfig: &StreamableHTTPConfig{
+					Port: 9000,
+				},
+			},
+			env: map[string]string{
+				"GENMCP_STREAMABLEHTTPCONFIG_PORT": "9000",
+			},
+		},
+		"surfaces error on invalid env var type": {
+			initialRuntime: &ServerRuntime{
+				TransportProtocol: "streamablehttp",
+				StreamableHTTPConfig: &StreamableHTTPConfig{
+					Port: 8080,
+				},
+			},
+			expectErr: true,
+			env: map[string]string{
+				"GENMCP_STREAMABLEHTTPCONFIG_PORT": "\"9000\"",
+			},
+		},
+	}
+
+	for tn, tc := range tt {
+		t.Run(tn, func(t *testing.T) {
+			for k, v := range tc.env {
+				os.Setenv(k, v)
+			}
+			defer func() {
+				for k := range tc.env {
+					os.Unsetenv(k)
+				}
+			}()
+
+			e := NewEnvRuntimeOverrider()
+
+			err := e.ApplyOverrides(tc.initialRuntime)
+			if tc.expectErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tc.expectedRuntime, tc.initialRuntime)
+			}
+
+		})
+	}
+}
